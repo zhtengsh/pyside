8719ce26ce03fead3cb037d7c1e44e5ec5b4b15a
zhtengsh <951841404@qq.com>
2016-02-10 15:58:59 +0800

    pyside

---
+++ baike_spider/html_downloader.py
@@ -0,0 +1,18 @@
+# coding:utf8
+import urllib2
+
+class HtmlDownloader(object):
+    """docstring for HtmlDownloader"""
+    def __init__(self):
+        super(HtmlDownloader, self).__init__()
+
+    def download(self, url):
+        if url is None:
+            return None
+
+        response = urllib2.urlopen(url)
+
+        if response.getcode() != 200:
+            return None
+
+        return response.read()
